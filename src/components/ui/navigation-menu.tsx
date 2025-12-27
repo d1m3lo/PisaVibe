26643115d80041ff0b0519486c52138cef6d750d
Manual Piuzinho <hokagebayyo@gmail.com>
2025-12-27 01:34:07 +0000

    estava passando o mouse em lançamentos e olha como está alinhado ofertas

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -85,7 +85,7 @@ const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-  <div className={cn("absolute left-0 top-full flex justify-start")}>
+  <div className={cn("absolute left-0 top-full flex")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
         "origin-top-left relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-b-md border-x-0 border-b bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",

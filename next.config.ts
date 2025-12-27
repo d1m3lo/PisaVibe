1638f0c9fa842d0cd63a4947317e1003e5dfc964
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2025-12-27 16:47:08 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'down-br.img.susercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

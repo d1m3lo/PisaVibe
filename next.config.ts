94567a5960d9219661e5bd2dab5f4358716952cc
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2025-12-28 00:41:43 +0000

    fix: Invalid src prop (https://i.ibb.co/jk1RFn1X/751d8b63-b2a0-49fe-b213

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'down-br.img.susercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
       },

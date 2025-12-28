4e370a1e255969535722a1ec20e636b94e9f4cd4
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2025-12-28 02:34:02 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://artwalk

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'artwalk.vtexassets.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

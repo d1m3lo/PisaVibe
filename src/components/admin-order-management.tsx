bbaa6bdf6d37e4aa8a5b38477fd5b95f57421e89
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2025-12-30 01:41:28 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/components/admin-order-management.tsx
+++ src/components/admin-order-management.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { collectionGroup, onSnapshot, query } from 'firebase/firestore';
 import { useFirestore } from '@/firebase';
 import {

b1041944eccd1dcc8f27feff38ea41d5c929bb16
Manual Piuzinho <hokagebayyo@gmail.com>
2025-12-31 00:14:23 +0000

    abaixe o max_width para 80%

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -265,7 +265,7 @@ export default function ProductPage() {
                           alt={`${displayName} - Imagem ${index + 1}`}
                           fill
                           className="object-cover mx-auto"
-                          style={{ maxWidth: '82%' }}
+                          style={{ maxWidth: '80%' }}
                           priority={index === 0}
                         />
                       </div>

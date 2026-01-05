13b51fbf2f987ee51ce594106243e76ef33e5449
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2026-01-05 13:32:23 +0000

    E outra na hora de logar com o google aparece isso né studio-4155277971-

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-4155277971-b1669",
   "appId": "1:569540051528:web:a6f66a95305804c0c70e7d",
   "apiKey": "AIzaSyAFQb1cGviY65znT6xlWcCMhnmVxFBsMD4",
-  "authDomain": "studio-4155277971-b1669.firebaseapp.com",
+  "authDomain": "pisavibe.shop",
   "measurementId": "",
   "messagingSenderId": "569540051528"
 };

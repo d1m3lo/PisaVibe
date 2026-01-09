c5874555a9701d009c00e1cc6ee2f7fdb006c410
Gabrielzin 762 <Demello.4000.NN@gmail.com>
2026-01-09 14:03:22 +0000

    Teria como você remover a subcategoria mochila tanto de masculino, quant

--- src/lib/menu-data.ts
+++ src/lib/menu-data.ts
@@ -43,7 +43,6 @@ export const megaMenuData: MenuCategory[] = [
         title: "Acessórios",
         links: [
           { title: "Bonés", href: "/produtos?genero=masculino&categoria=acessorios&tipo=bonés" },
-          { title: "Mochila", href: "/produtos?genero=masculino&categoria=acessorios&tipo=mochilas" },
           { title: "Relógios", href: "/produtos?genero=masculino&categoria=acessorios&tipo=relogios" },
         ],
       },
@@ -82,7 +81,6 @@ export const megaMenuData: MenuCategory[] = [
         title: "Acessórios",
         links: [
           { title: "Bolsas", href: "/produtos?genero=feminino&categoria=acessorios&tipo=bolsas" },
-          { title: "Mochilas", href: "/produtos?genero=feminino&categoria=acessorios&tipo=mochilas" },
         ],
       },
        {

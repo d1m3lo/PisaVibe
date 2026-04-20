7b0f369e05b485cc48b806c2f6b65a2f16cb96f4
Gabriel de Mello Rodrigues <gabrieldemello.rodrigues@gmail.com>
2026-04-20 02:05:40 -0300

    add

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "studio-4155277971-b1669",
+  "appId": "1:569540051528:web:a6f66a95305804c0c70e7d",
+  "apiKey": "REACT_APP_FIREBASE_API_KEY=xxxx",
+  "authDomain": "studio-4155277971-b1669.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "569540051528"
+};
+
